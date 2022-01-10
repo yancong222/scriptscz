010c2fda29f6209140d9ff5a3a478437e90c2409
yan cong <52762906+yancong222@users.noreply.github.com>
2022-01-10 17:41:58 -0500

    Update mfa.sh

--- acoustics/mfa/mfa.sh
+++ acoustics/mfa/mfa.sh
@@ -1,1 +1,7 @@
+for filename in /Users/xyz/* ; # optional
+do
+    justfile=$(basename -- "$filename") # optional
+    uid="${justfile%.*}" # optional
 
+    mfa align /path/to/librispeech/dataset /path/to/librispeech/lexicon.txt english~/Documents/aligned_librispeech ;
+done ;
